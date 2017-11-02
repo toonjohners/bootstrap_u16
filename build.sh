feb59c1acf0f828d3027bea610bfae1d3d6a45ee
John McCormack <johners@boggsport.com>
2017-11-02 09:50:20 +0000

    docker build

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ sudo apt-get install ansible git chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb 
 sudo apt-get install -f
-
+sudo apt-get install openssh-server
 
 #EOF
--- docker.sh
+++ docker.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update 
 apt-cache policy docker-ce
 sudo apt-get install -y docker-c
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+ 
+curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
